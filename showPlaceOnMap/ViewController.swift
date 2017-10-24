bdaff91488213334d249234d8835f53daf74c9c7
Edgar Skobichevskiy <4_ipx@mail.ru>
2017-10-24 18:43:40 +0800

    initial commit

--- showPlaceOnMap/ViewController.swift
+++ showPlaceOnMap/ViewController.swift
@@ -7,17 +7,79 @@
 //
 
 import UIKit
+import MapKit
+class ViewController: UIViewController , UISearchBarDelegate {
 
-class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    @IBAction func searchButton(_ sender: Any)
+    {
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchBar.delegate = self
+        present(searchController, animated: true, completion: nil)
+    }
+    
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        //ignoring user
+        UIApplication.shared.beginIgnoringInteractionEvents()
+        
+        //Activity indicator
+        let activityIndicator = UIActivityIndicatorView()
+        activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.gray
+        activityIndicator.center = self.view.center
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.startAnimating()
+        self.view.addSubview(activityIndicator)
+        
+        //hide search bar
+        searchBar.resignFirstResponder()
+        dismiss(animated: true, completion: nil)
+      
+        // create the search request
+        let searchRequest = MKLocalSearchRequest()
+        searchRequest.naturalLanguageQuery = searchBar.text
+        let activeSearch = MKLocalSearch(request: searchRequest)
+        
+        activeSearch.start { (responce, error) in
+            
+            activityIndicator.stopAnimating()
+            UIApplication.shared.endIgnoringInteractionEvents()
+            
+            if responce == nil {
+                print(error?.localizedDescription ?? "ERROR")
+            }
+            else {
+                // remove annotations
+                let annotations = self.mapView.annotations
+                self.mapView.removeAnnotations(annotations)
+                
+                // getting data
+                let latitude = responce?.boundingRegion.center.latitude
+                let longitude = responce?.boundingRegion.center.longitude
+                
+                // create annotation (pin)
+                let annotation = MKPointAnnotation()
+                annotation.title = searchBar.text
+                annotation.coordinate = CLLocationCoordinate2DMake(latitude!, longitude!)
+                self.mapView.addAnnotation(annotation)
+                
+                // Zooming an annotations
+                let coordinate = CLLocationCoordinate2DMake(latitude!, longitude!)
+                let span = MKCoordinateSpanMake(0.1, 0.1)
+                let region = MKCoordinateRegionMake(coordinate, span)
+                self.mapView.setRegion(region, animated: true)
+            }
+        }
+        
+        
+    }
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
